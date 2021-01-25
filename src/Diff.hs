516df65867015b1ccb400061917189fde8c2941a
Andrius Stankevičius <floppycat@gmail.com>
2021-01-25 20:05:53 +0200

    fst

--- src/Diff.hs
+++ src/Diff.hs
@@ -1,24 +1,31 @@
+{-# LANGUAGE TupleSections #-}
 {-# language ScopedTypeVariables #-}
+{-# language LambdaCase #-}
+{-# language GADTs #-}
 
 module Diff
 (
     BVar, BVarS, bvarValue,
     constant, var,
-    backprop, backpropS
+    backprop, backpropS,
+    fst
 )
 where
 
+import Expr(ExprArg(ArgExpr), Term3(Func2))
+import Prelude (Monad(return), Num, IO, ($))
 import Expr(ExprArg(ArgVar), Expr2(Expr2), Expr3(ExprSum), Term3(Func2))
 import Affine (AffineFunc(AffineFunc))
 import Data.VectorSpace (AdditiveGroup(zeroV))
-import Tensor (TensorProduct((⊗)), AFunction(IndentityFunc))
+import Tensor (TensorProduct((⊗)), AFunction(BlackBoxFunc, IndentityFunc))
 import NodeMap (runRecoverSharing5)
 
+import qualified Prelude
 import qualified Graph
 import qualified NodeMap
 import System.IO.Unsafe (unsafePerformIO)
 
-type BVar b a da v dv = AffineFunc b (Expr2 a da v dv)
+type BVar b a da v dv = AffineFunc b (ExprArg (Expr2 a da) a da v dv)
 
 type BVarS a = BVar a a a a a
 
@@ -26,17 +33,32 @@ bvarValue :: AffineFunc b dv -> b
 bvarValue (AffineFunc y0 _dy) = y0
 
 constant :: (AdditiveGroup v, AdditiveGroup dv) => b -> BVar b a da v dv
-constant x = AffineFunc x zeroV
+constant x = AffineFunc x (ArgExpr zeroV)
 
 var :: (AdditiveGroup v, AdditiveGroup dv) => b -> BVar b v dv v dv
-var x = AffineFunc x (Expr2 (ExprSum [Func2 IndentityFunc ArgVar]))
+var x = AffineFunc x ArgVar
 
-backprop :: forall b a da v dv. AdditiveGroup da => BVar b a da v dv -> dv -> da
-backprop (AffineFunc _y0 dy) dv = unsafePerformIO $ do
+backprop' :: forall a da v dv. AdditiveGroup da => Expr2 a da v dv -> dv -> da
+backprop' dy dv = unsafePerformIO $ do
     NodeMap.SomeSharedExprWithMap smap expr <- runRecoverSharing5 dy :: IO (NodeMap.SomeSharedExprWithMap a da v dv)
     let x' = Graph.convertGraph smap expr -- :: Graph.ForwardGraph s a da v dv
         dx' = Graph.flipGraph x' -- :: Graph.BackwardGraph s' a da v dv
     return (dv ⊗ dx')
 
+backprop :: forall b a da v dv. AdditiveGroup da => BVar b a da v dv -> dv -> da
+backprop (AffineFunc _y0 y) dv = case y of
+    ArgVar -> dv
+    ArgExpr x -> backprop' x dv
+
 backpropS :: (AdditiveGroup da, Num dv) => BVar b a da v dv -> da
 backpropS x = backprop x 1
+
+fstT :: AdditiveGroup dv => ExprArg (Expr2 a da) a da (u, v) (du, dv) -> Term3 (Expr2 a da) a da u du
+fstT x = Func2 (BlackBoxFunc Prelude.fst (, zeroV)) x
+
+fstA :: (AdditiveGroup u, AdditiveGroup du, AdditiveGroup  dv) => ExprArg (Expr2 a da) a da (u, v) (du, dv) -> ExprArg (Expr2 a da) a da u du
+fstA x = ArgExpr (Expr2 (ExprSum [fstT x]))
+
+fst :: (AdditiveGroup u, AdditiveGroup du, AdditiveGroup dv) => BVar (b1, b2) a da (u, v) (du, dv) -> BVar b1 a da u du
+fst (AffineFunc y0 dy) = AffineFunc (Prelude.fst y0) (fstA dy)
+
