8e840a7569bee5475b2d054ce601c90a9b3f93c8
Andrius Stankevičius <floppycat@gmail.com>
2021-01-30 11:38:27 +0200

    simplify

--- src/Sharing.hs
+++ src/Sharing.hs
@@ -26,7 +26,6 @@ import GHC.StableName
 import GHC.Exts (Any)
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as Map
-import Data.VectorSpace (AdditiveGroup(..))
 
 import Unsafe.Coerce (unsafeCoerce)
 import Control.Monad.Trans.State.Strict
@@ -40,15 +39,14 @@ newtype TreeBuilder f r = TreeCache { unTreeCache :: StateT (OpenMap f) IO r }
     deriving (Functor, Applicative, Monad)
 
 
-insertTreeCache :: (AdditiveGroup v, AdditiveGroup dv) => OpenKey v dv -> f v dv -> TreeBuilder f ()
+insertTreeCache :: OpenKey v dv -> f v dv -> TreeBuilder f ()
 insertTreeCache name value = do
     cache' <- TreeCache get
     let newCache = OpenMap.insert name value cache'
     TreeCache (put newCache)
 
 insertTreeBuilder
-    :: (AdditiveGroup v, AdditiveGroup dv)
-    => OpenKey v dv
+    :: OpenKey v dv
     -> TreeBuilder f (f v dv)
     -> TreeBuilder f (OpenKey v dv, f v dv)
 insertTreeBuilder name value = do
@@ -57,8 +55,7 @@ insertTreeBuilder name value = do
     return (name, y)
 
 insertTreeBuilder'
-    :: (AdditiveGroup v, AdditiveGroup dv)
-    => OpenKey v dv
+    :: OpenKey v dv
     -> TreeBuilder f (f v dv) -- ^ blah
     -> TreeBuilder f (OpenKey v dv, f v dv)
 insertTreeBuilder' name computeAction = do
@@ -67,16 +64,15 @@ insertTreeBuilder' name computeAction = do
         Just x -> return (name, x)
         Nothing -> insertTreeBuilder name computeAction
 
-newtype BuildAction f g = BuildAction (forall v dv. (AdditiveGroup v, AdditiveGroup dv) => f v dv -> TreeBuilder g (g v dv))
+newtype BuildAction f g = BuildAction (forall v dv. f v dv -> TreeBuilder g (g v dv))
 
 insertExpr
-  :: forall f g v dv. (AdditiveGroup v, AdditiveGroup dv)
-  => BuildAction f g
+  :: BuildAction f g
   -> f v dv
   -> TreeBuilder g (OpenKey v dv, g v dv)
 insertExpr (BuildAction value) expr = do
     name <- TreeCache (lift (OpenMap.makeOpenKey expr))
     insertTreeBuilder' name (value expr)
 
-runTreeBuilder :: (AdditiveGroup v, AdditiveGroup dv) => TreeBuilder f (g v dv) -> IO (g v dv, OpenMap f)
+runTreeBuilder :: TreeBuilder f (g v dv) -> IO (g v dv, OpenMap f)
 runTreeBuilder rs_x = runStateT (unTreeCache rs_x) OpenMap.empty
