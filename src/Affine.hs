be7c2feff46c22c4ac00df2f8a4c929d981dcb96
Andrius Stankevičius <floppycat@gmail.com>
2021-02-19 21:41:23 +0200

    floating instance

--- src/Affine.hs
+++ src/Affine.hs
@@ -17,6 +17,10 @@ import Data.VectorSpace (AdditiveGroup((^-^), negateV, zeroV), VectorSpace(Scala
 
 data AffineFunc b dv = AffineFunc b dv
 
+class VectorSpace dv => LinearFunc dv where
+    identityFunc :: dv
+    scaleFunc :: Scalar dv -> dv -- scaleFunc x == x *^ identityFunc
+
 evalAffineFunc :: (AdditiveGroup (dv ⊗ v), TensorProduct dv v) => AffineFunc (dv ⊗ v) dv -> v -> dv ⊗ v
 evalAffineFunc (AffineFunc y0 dydx) x = y0 ^+^ (dydx ⊗ x)
 
@@ -39,6 +43,32 @@ instance (Num b, VectorSpace dv, b ~ Scalar dv) => Num (AffineFunc b dv) where
     signum (AffineFunc f0 _) = AffineFunc (signum f0) zeroV
     fromInteger x = AffineFunc (fromInteger x) zeroV
 
+sqr :: Num a => a -> a  -- TODO: inefficient squaring
+sqr x = x*x
+
+rsqrt :: Floating a => a -> a
+rsqrt x = recip (sqrt x)
+
+instance (Fractional b, VectorSpace dv, b ~ Scalar dv) => Fractional (AffineFunc b dv) where
+    fromRational x = AffineFunc (fromRational x) zeroV
+    recip (AffineFunc x dx) = AffineFunc (recip x) (df *^ dx)
+        where df = negate (recip (sqr x))
+    -- TODO: implement division
+
+instance (Floating b, VectorSpace dv, b ~ Scalar dv) => Floating (AffineFunc b dv) where
+    pi = AffineFunc pi zeroV
+    exp (AffineFunc x dx) = AffineFunc (exp x) (exp x *^ dx)
+    log (AffineFunc x dx) = AffineFunc (log x) (recip x *^ dx)
+    sin (AffineFunc x dx) = AffineFunc (sin x) (cos x *^ dx)
+    cos (AffineFunc x dx) = AffineFunc (cos x) (negate (sin x) *^ dx)
+    asin (AffineFunc x dx) = AffineFunc (asin x) ((rsqrt (1 - sqr x)) *^ dx)
+    acos (AffineFunc x dx) = AffineFunc (acos x) (negate (rsqrt (1 - sqr x)) *^ dx)
+    atan (AffineFunc x dx) = AffineFunc (atan x) (recip (1 + sqr x) *^ dx)
+    sinh (AffineFunc x dx) = AffineFunc (sinh x) (cosh x *^ dx)
+    cosh (AffineFunc x dx) = AffineFunc (cosh x) (sinh x *^ dx)
+    asinh (AffineFunc x dx) = AffineFunc (asinh x) (rsqrt (1 + sqr x) *^ dx)
+    acosh (AffineFunc x dx) = AffineFunc (acosh x) (rsqrt (sqr x - 1) *^ dx)
+    atanh (AffineFunc x dx) = AffineFunc (atanh x) (recip (1 - sqr x) *^ dx)
 
 --instance () => Num (AffineFunc b (Expr3 p a da v dv)) where
 --    (AffineFunc f0 df) + (AffineFunc g0 dg) = AffineFunc (f0+g0) (df ^+^ dg)
