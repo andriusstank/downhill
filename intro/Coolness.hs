49f97bd0302ec8f8f6b3ce6e5e3407cc747b3bb1
Andrius Stankevičius <floppycat@gmail.com>
2021-05-14 21:49:01 +0300

    naming

--- intro/Coolness.hs
+++ intro/Coolness.hs
@@ -1,23 +1,17 @@
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
-
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
-
-
 {-# LANGUAGE DerivingVia #-}
+
 module Coolness
 where
 import Data.AdditiveGroup (sumV, AdditiveGroup)
---import Tensor (AFunction, Bilinear(..), Vec(..), Bilinear'', transposeFunc)
 import Data.Kind (Type)
 
 
-class Bilinear u v where
+class TensorMul u v where
   type u ✕ v :: Type
   (✕) :: u -> v -> u ✕ v
 
@@ -25,57 +19,56 @@ newtype Vec dx = Vec { unVec :: dx }
     deriving Show
     deriving AdditiveGroup via dx
 
---type family Vec dx where
-    --Vec dx = dx
 
-data AFunction u du v dv where
-    BlackBoxFunc :: (u -> v) -> (dv -> du) -> AFunction u du v dv
+data BlackBoxFunc u du v dv = BlackBoxFunc (u -> v) (dv -> du)
+
+instance TensorMul (BlackBoxFunc u du v dv) (Vec u) where
+    type (BlackBoxFunc u du v dv) ✕ (Vec u) = Vec v
+    (BlackBoxFunc f _) ✕ Vec v = Vec (f v)
+
+instance TensorMul (Vec dv) (BlackBoxFunc u du v dv) where
+    type (Vec dv) ✕ (BlackBoxFunc u du v dv) = Vec du
+    Vec v ✕ (BlackBoxFunc _ f) = Vec (f v)
+
 
 data Expr a da v dv where
-    Variable :: Expr a da a da
-    Func :: (AdditiveGroup u, AdditiveGroup v, AdditiveGroup du, AdditiveGroup dv) => AFunction u du v dv -> Expr a da u du -> Expr a da v dv
+    Var :: Expr a da a da
+    Func :: BlackBoxFunc u du v dv -> Expr a da u du -> Expr a da v dv
     Sum :: AdditiveGroup v => [Expr a da v dv] -> Expr a da v dv
 
 -- Evaluate
-instance Bilinear (Expr a da v dv) (Vec a) where
+instance TensorMul (Expr a da v dv) (Vec a) where
     type Expr a da v dv ✕ Vec a = Vec v
     expr ✕ a = case expr of
-        Variable -> a
-        Func f x -> f ✕ (x ✕ a)
-        Sum xs -> sumV [x ✕ a | x <- xs]
+        Var -> a
+        Func f v -> f ✕ (v ✕ a)
+        Sum vs -> sumV [v ✕ a | v <- vs]
 
-instance Bilinear (AFunction u du v dv) (Vec u) where
-    type (AFunction u du v dv) ✕ (Vec u) = Vec v
-    (BlackBoxFunc f _) ✕ Vec x = Vec (f x)
-
-instance Bilinear (Vec dv) (AFunction u du v dv) where
-    type (Vec dv) ✕ (AFunction u du v dv) = Vec du
-    Vec x ✕ (BlackBoxFunc _ f) = Vec (f x)
-
--- Substitute
-instance Bilinear (Expr x dx v dv) (Expr a da x dx) where
-    type Expr x dx v dv ✕ Expr a da x dx = Expr a da v dv
-    expr ✕ x = case expr of
-        Variable -> x
-        Func f y -> Func f (y ✕ x)
-        Sum ys -> Sum [y ✕ x | y <- ys]
 
 -- Reverse mode evaluation
-instance AdditiveGroup da => Bilinear (Vec dv) (Expr a da v dv) where
+instance AdditiveGroup da => TensorMul (Vec dv) (Expr a da v dv) where
     type Vec dv ✕ (Expr a da v dv) = Vec da
     dv ✕ expr = case expr of
-        Variable -> dv
-        Func f x -> (dv ✕ f) ✕ x
-        Sum xs -> sumV [dv ✕ x | x <- xs]
+        Var -> dv
+        Func f v -> (dv ✕ f) ✕ v
+        Sum vs -> sumV [dv ✕ v | v <- vs]
+    
+-- Substitute
+instance TensorMul (Expr x dx v dv) (Expr a da x dx) where
+    type Expr x dx v dv ✕ Expr a da x dx = Expr a da v dv
+    expr ✕ v = case expr of
+        Var -> v
+        Func f y -> Func f (y ✕ v)
+        Sum ys -> Sum [y ✕ v | y <- ys]
 
-transposeFunc :: AFunction u v du dv -> AFunction dv du v u
-transposeFunc = \case
-    BlackBoxFunc f g -> BlackBoxFunc g f
+
+transposeFunc :: BlackBoxFunc u v du dv -> BlackBoxFunc dv du v u
+transposeFunc (BlackBoxFunc f g) =  BlackBoxFunc g f
 
 transposeExpr :: AdditiveGroup da => Expr a da v dv -> Expr dv v da a
 transposeExpr = \case
-    Variable -> Variable
-    Func f x -> x' ✕ f'
-        where x' = transposeExpr x
-              f' = Func (transposeFunc f) Variable
+    Var -> Var
+    Func f v -> v' ✕ f'
+        where v' = transposeExpr v
+              f' = Func (transposeFunc f) Var
     Sum xs -> Sum (transposeExpr <$> xs)
