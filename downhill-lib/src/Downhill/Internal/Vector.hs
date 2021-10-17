883c054ead9d941cbfdab2271586fd2ae96dcbd0
Andrius Stankevičius <floppycat@gmail.com>
2021-10-17 15:59:54 +0300

    sumBuilderV

--- downhill-lib/src/Downhill/Internal/Vector.hs
+++ downhill-lib/src/Downhill/Internal/Vector.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -7,10 +8,9 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
-{-# LANGUAGE FlexibleInstances #-}
 
 module Downhill.Internal.Vector
-  (
+  ( Slice, sumBuilderV
   )
 where
 
@@ -21,11 +21,26 @@ import qualified Data.Vector as Unsized
 import qualified Data.Vector.Generic.Mutable as UnsizedMutable
 import Data.Vector.Generic.Mutable.Sized (fromSized, set, unsafeNew)
 import qualified Data.Vector.Generic.Sized as VGS
---import Data.Vector.Sized
 import Downhill.Linear.Expr (BasicVector (VecBuilder, sumBuilder))
 import GHC.TypeLits (KnownNat, Nat)
 
 data Slice (n :: Nat) a = Slice Int Int (Unsized.Vector (VecBuilder a))
+
+sumBuilderV :: forall n a. (KnownNat n, BasicVector a) => DList (Slice n a) -> VGS.Vector Unsized.Vector n a
+sumBuilderV slices = sumBuilder <$> runST mbuilders
+  where
+    mbuilders :: forall s. ST s (VGS.Vector Unsized.Vector n (VecBuilder a))
+    mbuilders = do
+      x <- unsafeNew
+      set x mempty
+      let v = fromSized x
+          addSlice :: Slice n a -> ST s ()
+          addSlice (Slice from count builders) = traverse_ bump [0 .. count - 1]
+            where
+              bump :: Int -> ST s ()
+              bump i = UnsizedMutable.modify v (<> builders Unsized.! i) (from + i)
+      traverse_ addSlice slices
+      VGS.unsafeFreeze x
 
 instance
   ( KnownNat n,
@@ -34,20 +49,5 @@ instance
   BasicVector (VGS.Vector Unsized.Vector n a)
   where
   type VecBuilder (VGS.Vector Unsized.Vector n a) = DList (Slice n a)
-  sumBuilder slices = sumBuilder <$> runST mbuilders
-    where
-      mbuilders :: forall s. ST s (VGS.Vector Unsized.Vector n (VecBuilder a))
-      mbuilders = do
-        x <- unsafeNew
-        set x mempty
-        let v = fromSized x
-            addSlice:: Slice n a -> ST s ()
-            addSlice (Slice from count builders) = traverse_ bump [0 .. count -1]
-              where
-                bump :: Int -> ST s ()
-                bump i = UnsizedMutable.modify v (<> builders Unsized.! i) (from + i)
-        traverse_ addSlice slices
-        VGS.unsafeFreeze x
+  sumBuilder = sumBuilderV
 
---let x = Vector.replicate mempty :: Vector v n (VecBuilder a)
---unsafeFreeze x
